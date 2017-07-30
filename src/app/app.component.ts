afc9a95899ea67bca308d95b9567d31bad6d61b4
SenorJos <joshuavanmeer@gmail.com>
2017-07-30 16:52:18 +0200

    lesson1 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 ﻿import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private title:boolean = true;
+    
+    title: string = 'Tour of Heroes';
+    hero: Hero = {
+        id: 0,
+        name: 'WindStorm'
+    };
+    
+
+          
+
 }
